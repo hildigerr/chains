83012fa179f5958927ca2ebd9812dea174455ede
Hildigerr Vergaray <Roberto.Vergaray@YmirSystems.com>
2019-09-17 16:17:00 -0700

    Enable using chains like ropes.

--- init.lua
+++ init.lua
@@ -10,6 +10,19 @@ minetest.register_node("chains:chain", {
     inventory_image = "Chain_inv.png",
     tile_images = { "Chain.png" },
     drawtype = "plantlike",
+    on_place = place_rope,
+    after_dig_node = function(pos, oldnode, oldmetadata, digger)
+        local num = 0
+        local below = {x=pos.x,y=pos.y -1,z=pos.z}
+        while minetest.env:get_node(below).name == "chains:chain" do
+            minetest.env:remove_node(below)
+            below.y = below.y -1
+            num = num + 1
+        end
+        if num ~= 0 then
+            digger:get_inventory():add_item("main", "chains:chain "..num)
+        end
+    end,
     groups = {cracky=3},
     sounds =  default.node_sound_stone_defaults(),
     })
